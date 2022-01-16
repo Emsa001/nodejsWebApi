bb6a6f83607f585e142d807319dbe140a03948a9
Emsa001 <emikscur123@gmail.com>
2022-01-16 19:23:32 +0100

    main

--- webapi/index.js
+++ webapi/index.js
@@ -34,19 +34,66 @@ const app = express();
 
 app.use(cors());
 
-/*app.get("/create-movie", async (req, res) => {
+app.post("/movies", async (req, res) => {
+  if (!req.query.title || !req.query.date || !req.query.points)
+    return res.json({
+      Error: `Undefned title, date and points`,
+    });
   const movie = await Movies.create({
-    title: `Movie 1`,
-    releaseDate: "19.05.2005",
+    title: req.query.title,
+    releaseDate: req.query.date,
     image:
-      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTfyoVPjY_kCTpEDw7oUySjrgBP25iiC0DxfICJ5VH4SkuVlWHI",
-    points: 7.6,
+      req.query.image ||
+      "https://wallpapers.com/images/high/grey-white-gradient-vertical-cover-36b0dza0un01a5ss.jpg",
+    points: req.query.points,
   });
-  res.json(movie);
-});*/
+  res.json({ message: `Sucessfully created movie ${req.query.title}`, movie });
+});
 
-app.get("/", async (req, res) => {
+app.delete("/movies", async (req, res) => {
+  let s_movie = await Movies.findOne({ where: { id: req.query.id || -1 } });
+  if (!s_movie)
+    return res.json({
+      Error: `Not found movie with id: ${req.query.id || "underfined"}`,
+    });
+
+  const movie = await Movies.destroy({
+    where: {
+      id: req.query.id,
+    },
+    force: true,
+  });
+  res.json({ message: `Sucessfully deleted movie ${req.query.id}`, movie });
+});
+
+app.put("/movies", async (req, res) => {
+  let s_movie = await Movies.findOne({ where: { id: req.query.id || -1 } });
+  if (!s_movie)
+    return res.json({
+      Error: `Not found movie with id: ${req.query.id || "underfined"}`,
+    });
+  const movie = await Movies.update(
+    {
+      title: req.query.title || s_movie.title,
+      releaseDate: req.query.date || s_movie.date,
+      image: req.query.image || s_movie.image,
+      points: req.query.points || s_movie.points,
+    },
+    {
+      where: {
+        id: req.query.id,
+      },
+    }
+  );
+  res.json({
+    message: `Sucessfully updated movie ${req.query.id}`,
+    movie: await Movies.findOne({ where: { id: req.query.id } }),
+  });
+});
+
+app.get("/movies", async (req, res) => {
   const movies = await Movies.findAll();
   res.json(movies);
 });
+
 app.listen(5555);
